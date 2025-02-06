67421482320107262c397ed66f262b5a1703070b
Chandelkar51 <nikhil05052001@gmail.com>
2025-02-06 19:35:09 +0530

    maked functional

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
+const hBurg=document.querySelector(".hamburg");
 function hamburg() {
     const elm=document.getElementsByClassName("dropdown");
     elm[0].style.transform="translateY(0)";
+    hBurg.style.display="none";
 }
 function cancel() {
     const elm=document.querySelector(".dropdown");
-    // console.log(elm.style);
     elm.style.transform="translateY(-300px)";
     elm.style.transition= ".2s linear";
+    hBurg.style.display="inline";
 }
 
-const text=["FULLSTACK", "DEVELOPER"];
+const text=["FULLSTACK DEVELOPER", "Programer"];
 let speed=100;
 const textElm=document.querySelector("#typewriter-text");
 let textIndex=0;
 let char=0;
-// console.log(textElm);
+
 function typeWriter(){
     if(char < text[textIndex].length){
         textElm.innerHTML+=text[textIndex].charAt(char);
@@ -22,12 +24,12 @@ function typeWriter(){
         setTimeout(typeWriter, speed);
     }
     else
-        setTimeout(eraseText, 1000);
+        setTimeout(eraseText, 500);
 }
 function eraseText(){
     if(textElm.innerHTML.length > 0){
         textElm.innerHTML =textElm.innerHTML.slice(0, -1);
-        setTimeout(eraseText, 50);
+        setTimeout(eraseText, 30);
     }
     else{
         textIndex=(textIndex+1)%text.length;
